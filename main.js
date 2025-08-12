1c49efb55f0262660fa146759415c336f7b36c4b
JOÃO VICTOR SANTOS DE MORAES <25132878@CFP127ALUNO.LOCAL>
2025-08-12 08:45:02 -0300

    projeto finalizado, falta dar uma ajeitada no codigo

--- main.js
+++ main.js
@@ -3,42 +3,50 @@
 const imagens = [
     {
         nome: 'Imagem 1',
-        url: './img/natureza1.jpg'
+        url: './img/natureza1.jpg',
+        text: 'Névoa na floresta'
     },
 
     {
         nome: 'Imagem 2',
-        url: './img/natureza2.jpg'
+        url: './img/natureza2.jpg',
+        text: 'Prados verdosos'
     },
 
     {
         nome: 'Imagem 3',
-        url: './img/natureza3.jpg'
+        url: './img/natureza3.jpg',
+        text: 'Sapinho na folhinha'
     },
 
     {
         nome: 'Imagem 4',
-        url: './img/natureza4.jpg'
+        url: './img/natureza4.jpg',
+        text: 'Campos ao nascer do sol'
     },
 
     {
         nome: 'Imagem 5',
-        url: './img/natureza5.jpg'
+        url: './img/natureza5.jpg',
+        text: 'Cachoeira nas pedras musgosas'
     },
 
     {
         nome: 'Imagem 6',
-        url: './img/natureza6.jpg'
+        url: './img/natureza6.jpg',
+        text: 'Cachoeira nas rochas'
     },
 
     {
         nome: 'Imagem 7',
-        url: './img/natureza7.jpg'
+        url: './img/natureza7.webp',
+        text: 'Esquilo procurando nozes'
     },
 
     {
         nome: 'Imagem 8',
-        url: './img/natureza8.jpg'
+        url: './img/natureza8.jpg',
+        text: 'Vegetação na floresta'
     }
 ]
 
@@ -46,13 +54,13 @@ function criarImagem(srcImagem) {
     const galeria = document.getElementById('galeria')
     const div = document.createElement('div')
     const imagem = document.createElement('img')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('teste')
+    div.classList.add('divImg')
     imagem.src = srcImagem.url
+    paragrafo.textContent = srcImagem.text
     galeria.appendChild(div)
-    imagem.style.marginBottom = '0px'
-    imagem.style.marginLeft = '0'
-    imagem.style.backgroundColor = 'black'
-    imagem.style.color = 'white'
-    imagem.style.content = 'ahhhhhhh'
+    div.appendChild(paragrafo)
     div.appendChild(imagem)
 }
 
